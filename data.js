4f8993a036cd3a3517eb86cd7bd1a2f99e2d1edd
cuong28021999 <cuongsdtq1999@gmail.com>
2020-07-26 12:29:12 +0700

    Demo website

---
+++ data.js
@@ -0,0 +1,63 @@
+const data= [
+   {
+      "answerIndex":3,
+      "answer":[
+         "4",
+         "5",
+         "6",
+         "7"
+      ],
+      "description":[
+         "Chưa có dữ liệu"
+      ],
+      "questionContent":"Từ \"伽\" - \"nhà\" có mấy nét?"
+   },
+   {
+      "questionContent":"Từ \"多\" có nghĩa là",
+      "answerIndex":0,
+      "description":[
+         "1. (Tính) Nhiều. ◇Luận Ngữ 論語: “Hữu trực, hữu lượng, hữu đa văn, ích hĩ” 友直, 友諒, 友多聞, 益矣 (Quý thị 季氏) Bạn chính trực, bạn thành tín, bạn có nhiều kiến thức, (là ba thứ bạn) có ích vậy.",
+         "2. (Tính) Dư, hơn. ◎Như: “nhất niên đa” 一年多 một năm dư, “thập vạn đa nhân” 十萬多人 hơn mười vạn người. ◇Thủy hử truyện 水滸傳: “Tam nhị lí đa lộ, khán khán cước toan thối nhuyễn, chánh tẩu bất động, khẩu lí bất thuyết, đỗ lí trù trừ” 三二里多路, 看看腳酸腿軟, 正走不動, 口里不說, 肚里躊躇 (Đệ nhất hồi) Đi hơn vài dặm, thì thấy chân đau đùi mỏi, bước lên không được nữa, miệng không nói ra (nhưng) trong bụng đã thấy ngần ngại.",
+         "3. (Động) Khen ngợi, xưng tán. ◎Như: “đa kì hữu lễ” 多其有禮 khen người có lễ lắm. ◇Sử Kí 史記: “Đương thị thì, chư công giai đa Quý Bố năng tồi cương vi nhu, Chu Gia diệc dĩ thử danh văn đương thế” 當是時, 諸公皆多季布能摧剛為柔, 朱家亦以此名聞當世 (Quý Bố truyện 季布傳) Bấy giờ mọi người đều khen Quý Bố là đã khiến được con người sắt đá trở nên yếu mềm, Chu Gia cũng nhân việc này mà nổi tiếng với đời.",
+         "4. (Động) Thắng, vượt hơn. ◇Nguyễn Trãi 阮廌: “Ngâm ông thùy dữ thế nhân đa” 吟翁誰與世人多 (Hí đề 戲題) Nhà thơ với người đời, ai hơn?",
+         "5. (Phó) Chỉ, chỉ là. § Cũng như chữ “chỉ” 只. ◇Luận Ngữ 論語: “Đa kiến kì bất tri lượng dã” 多見其不知量也 (Tử Trương 子張) Chỉ thấy mà không biết liệu xét vậy.",
+         "6. (Phó) Phần nhiều, phần lớn. ◇Tả truyện 左傳: “Đại phu đa tiếu chi, duy Án Tử tín chi” 大夫多笑之, 唯晏子信之 (Chiêu Công 昭公) Các đại phu phần nhiều đều cười ông ta, chỉ có Án Tử là tin thôi."
+      ],
+      "answer":[
+         "đa",
+         "đi",
+         "liêm",
+         "lý "
+      ]
+   },
+   {
+      "questionContent":"  Từ \"nước\" (đất nước) viết là",
+      "answer":[
+         "去",
+         "多",
+         "伽",
+         "匿"
+      ],
+      "description":[
+         "Chưa cập nhật"
+      ],
+      "answerIndex":3
+   },
+   {
+      "questionContent":"Từ \"去\" có nghĩa là",
+      "answerIndex":1,
+      "answer":[
+         "khải",
+         "khứ",
+         "trần",
+         "bá"
+      ],
+      "description":[
+         "1. (Động) Đi. § Tự đây mà đi tới kia gọi là “khứ” 去, đối với “lai” 來. ◎Như: “khứ học hiệu” 去學校 đi tới trường. ◇Lí Bạch 李白: “Công thành khứ Ngũ Hồ” 功成去五湖 (Tặng Vi bí thư Tử Xuân 贈韋秘書子春) Công nghiệp đã làm nên, đi (ngao du) Ngũ Hồ.",
+         "2. (Động) Từ bỏ, rời bỏ. ◎Như: “khứ chức” 去職 từ bỏ chức vụ, “khứ quốc” 去國 rời xa đất nước.",
+         "3. (Động) Chết. ◇Thang Hiển Tổ 湯顯祖: “Đỗ Nha tiểu thư khứ tam niên” 擇日展禮 (Mẫu đan đình 牡丹亭) Tiểu thư Đỗ Nha mất đã ba năm.",
+         "4. (Động) Cách khoảng. ◎Như: “khứ cổ dĩ viễn” 去古已遠 đã cách xa với cổ xưa. ◇Lí Bạch 李白: “Liên phong khứ thiên bất doanh xích” 連峰去天不盈尺 (Thục đạo nan 蜀道難) Những ngọn núi liền nhau cách trời không đầy một thước.",
+         "5. (Động) Vứt bỏ. ◎Như: “khứ thủ” 去取 bỏ và lấy. ◇Trang Tử 莊子: “Li hình khứ trí” 離形去知 (Đại tông sư 大宗師) Lìa hình vứt trí. ◇Hán Thư 漢書: “Đắc Hán thực vật giai khứ chi” 得漢食物皆去之 (Hung nô truyện thượng 匈奴傳上) Được đồ ăn của Hán đều vứt đi."
+      ]
+   }
+]
